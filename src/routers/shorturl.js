04f6e585fafa8b0dc1ebd76a353b75cde3a00bf9
Alon Levian <levianalon@gmail.com>
2021-05-05 14:13:05 +0300

    Fix dns lookup

--- src/routers/shorturl.js
+++ src/routers/shorturl.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const validator = require('validator');
 const dns = require('dns');
 const Url = require('../models/url');
 const router = express.Router();
@@ -11,32 +10,33 @@ router.post('/api/shorturl', async (req, res) => {
         return res.status(400).send();
     }
 
-    dns.lookup(url, (error) => {
+    dns.lookup(url, {}, async (error) => {
+        console.log(error);
         if (error) {
-            return res.status(400).send({error: 'invalid url'});
+            return res.status(400).send({ error: 'invalid url' });
         }
-    })
 
-    const urlDocument = await Url.findOne({ original_url: url });
+        const urlDocument = await Url.findOne({ original_url: url });
 
-    if (urlDocument) {
-        return res.send(urlDocument);
-    }
+        if (urlDocument) {
+            return res.send(urlDocument);
+        }
 
-    const lastDocumentNumber = await Url.getLastDocumentNumber();
+        const lastDocumentNumber = await Url.getLastDocumentNumber();
 
-    try {
-        const addedUrl = new Url({
-            original_url: url,
-            short_url: lastDocumentNumber + 1
-        });
+        try {
+            const addedUrl = new Url({
+                original_url: url,
+                short_url: lastDocumentNumber + 1
+            });
 
-        await addedUrl.save();
+            await addedUrl.save();
 
-        res.send(addedUrl);
-    } catch (error) {
-        res.status(500).send({ error });
-    }
+            res.send(addedUrl);
+        } catch (error) {
+            res.status(500).send({ error });
+        }
+    });
 });
 
 router.get('/api/shorturl/:short_url', async (req, res) => {
